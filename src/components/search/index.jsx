e11c848ed92a324a7979e20322de6774bdf7f34e
Amit Chawla <amit.chawla2@sapient.com>
2020-06-04 21:00:18 +0530

    Added search action

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { fetchCharacters } from "../../redux/actions";
 
-const Search = props => {
+const Search = ({ search }) => {
+  const [value, setValue] = useState("");
+
+  const handleOnChange = event => {
+    setValue(event.target.value);
+  };
+
   return (
     <div class="input-group">
-      <input type="text" class="form-control" placeholder="Search..." />
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Search..."
+        value={value}
+        onChange={handleOnChange}
+      />
       <div class="input-group-append">
         <button
           class="btn btn-outline-secondary"
           type="button"
           id="button-addon2"
+          onClick={() => search(value)}
         >
           Search
         </button>
@@ -17,4 +32,8 @@ const Search = props => {
   );
 };
 
-export default Search;
+const mapDispatchToProps = dispatch => ({
+  search: searchTerm => dispatch(fetchCharacters(searchTerm))
+});
+
+export default connect(null, mapDispatchToProps)(Search);
